eb5091e3b547d46075fd9d4f398d079eeecb934a
shulksteve <shulk.work@gmail.com>
2025-09-15 19:55:22 +0800

    update

--- src/app/api/emails/messages/route.ts
+++ src/app/api/emails/messages/route.ts
@@ -13,9 +13,12 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ msg: '数据异常', code: 400 }, { status: 400 });
   }
   try {
+    // 限制10分钟内的邮件
+    const time_limit = new Date(Date.now() - 10 * 60 * 1000).toISOString();
+
     const result = await pool.query(
-      "SELECT uuid, from_name, from_email, to_name, to_email, subject, summary, created_at  FROM emails WHERE to_email = $1 AND created_at >= NOW() - interval '10 minutes' ORDER BY created_at DESC LIMIT 100",
-      [email],
+      'SELECT uuid, from_name, from_email, to_name, to_email, subject, summary, created_at  FROM emails WHERE to_email = $1 AND created_at >= $2 ORDER BY created_at DESC LIMIT 100',
+      [email, time_limit],
     );
 
     return NextResponse.json({
